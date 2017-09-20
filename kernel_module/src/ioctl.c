8b4409e338df19bf20dffbe3484e90a17d0ba3e4
Animesh Sinsinwal <assinsin@ncsu.edu>
2017-09-20 01:48:38 -0400

    updating minor changes

--- kernel_module/src/ioctl.c
+++ kernel_module/src/ioctl.c
@@ -45,14 +45,14 @@
 
 // If exist, return the data.
 
-//mutex that will be shared among the threads
-static DEFINE_MUTEX(k_mutex); 
+// mutex that will be shared among the threads
+// static DEFINE_MUTEX(k_mutex); 
 
 long npheap_lock(struct npheap_cmd __user *user_cmd)
 {
     printk("Calling npheap_lock function.");
     struct mutex obj_lock;
-    obj_lock = getLock(user_cmd->offset); 
+    obj_lock = getMutex((__u64) user_cmd->offset); 
     mutex_lock(&obj_lock);
     return 0;
 }     
@@ -61,16 +61,16 @@ long npheap_unlock(struct npheap_cmd __user *user_cmd)
 {
     printk("Calling npheap_unlock function.");
     struct mutex obj_lock;
-    obj_lock = getLock(user_cmd->offset); 
+    obj_lock = getMutex((__u64) user_cmd->offset); 
     mutex_unlock(&obj_lock);
     return 0;
 }
 
 long npheap_getsize(struct npheap_cmd __user *user_cmd)
 {
-    printk("Calling npheap_getsize function.")
+    printk("Calling npheap_getsize function.");
     struct node object;
-    object = getObject(user_cmd->offset);
+    object = getObject((__u64) user_cmd->offset);
     return (long) object.size;
 }
 
@@ -78,8 +78,8 @@ long npheap_getsize(struct npheap_cmd __user *user_cmd)
 long npheap_delete(struct npheap_cmd __user *user_cmd)
 {
     printk("Calling npheap_delete function.");
-    struct node *object;
-    *object = getObject(user_cmd->offset);
+    struct node object;
+    object = getObject((__u64) ser_cmd->offset);
 
     object->size = 0;
     object->k_virtual_addr = NULL;
