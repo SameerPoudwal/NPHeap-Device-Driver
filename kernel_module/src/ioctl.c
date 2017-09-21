8dbb8bc9600bf9fba0099c35f98584b179009d3f
Animesh Sinsinwal <assinsin@ncsu.edu>
2017-09-20 21:26:26 -0400

    return type set

--- kernel_module/src/ioctl.c
+++ kernel_module/src/ioctl.c
@@ -77,16 +77,16 @@ long npheap_unlock(struct npheap_cmd __user *user_cmd)
 long npheap_getsize(struct npheap_cmd __user *user_cmd)
 {
     printk("Calling npheap_getsize function. \n");
-    struct node *object;
+    __u64 size;
     struct npheap_cmd copy;
     if(copy_from_user(&copy, (void __user *)user_cmd, sizeof(struct npheap_cmd)))
-        object = getSize(copy.offset/PAGE_SIZE);
+        size = getSize(copy.offset/PAGE_SIZE);
+        return size;
     else        
         return -EFAULT;
 
     //object = getObject((__u64) user_cmd->offset);
     //return (long) object->size;
-    return 0;
 }
 
 
