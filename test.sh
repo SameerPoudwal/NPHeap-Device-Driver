8de4664f0f40881fcb42cb9f7d5416219fcf211f
asinsinwal <assinsin@ncsu.edu>
2017-09-21 19:47:22 -0400

    scdc

--- test.sh
+++ test.sh
@@ -16,9 +16,9 @@ sudo make validate
 cd ..
 sudo insmod kernel_module/npheap.ko
 sudo chmod 777 /dev/npheap
-./benchmark/benchmark 32 8192 2
+./benchmark/benchmark 10 8192 3
 cat *.log > trace
 sort -n -t 3 trace > sorted_trace
-./benchmark/validate 32 < sorted_trace
+./benchmark/validate 10 < sorted_trace
 #rm -f *.log
 sudo rmmod npheap
