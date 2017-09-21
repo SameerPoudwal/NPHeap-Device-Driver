c517db3e6ce40e740fe3df9daab4e86aac79a911
Animesh Sinsinwal <assinsin@ncsu.edu>
2017-09-20 20:10:15 -0400

    to run benchmark only

--- test.sh
+++ test.sh
@@ -17,8 +17,8 @@ cd ..
 sudo insmod kernel_module/npheap.ko
 sudo chmod 777 /dev/npheap
 ./benchmark/benchmark 64 64 1
-cat *.log > trace
-sort -n -t 3 trace > sorted_trace
-./benchmark/validate 256 < sorted_trace
-rm -f *.log
-sudo rmmod npheap
+#cat *.log > trace
+#sort -n -t 3 trace > sorted_trace
+#./benchmark/validate 256 < sorted_trace
+#rm -f *.log
+#sudo rmmod npheap
