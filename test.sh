64a5cf6f795d76fdcac77aae241b320ff429ce15
ssgodbol <ssgodbol@ncsu.edu>
2017-09-21 14:39:27 -0400

    less values

--- test.sh
+++ test.sh
@@ -16,9 +16,9 @@ sudo make validate
 cd ..
 sudo insmod kernel_module/npheap.ko
 sudo chmod 777 /dev/npheap
-./benchmark/benchmark 1024 8192 40
-#cat *.log > trace
-#sort -n -t 3 trace > sorted_trace
-#./benchmark/validate 256 < sorted_trace
-#rm -f *.log
-#sudo rmmod npheap
+./benchmark/benchmark 64 64 2  
+cat *.log > trace
+sort -n -t 3 trace > sorted_trace
+./benchmark/validate 256 < sorted_trace
+rm -f *.log
+sudo rmmod npheap
